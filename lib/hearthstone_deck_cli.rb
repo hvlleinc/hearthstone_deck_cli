070026e895d744edac72a4f194aaee91f1a261c6
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2021-02-03 18:58:37 -0500

    first commit

---
+++ lib/hearthstone_deck_cli.rb
@@ -0,0 +1,7 @@
+require "pry"
+require "hearthstone_deck_cli/version"
+
+module HearthstoneDeckCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hearthstone_deck_cli/version.rb
@@ -0,0 +1,3 @@
+module HearthstoneDeckCli
+  VERSION = "0.1.0"
+end
