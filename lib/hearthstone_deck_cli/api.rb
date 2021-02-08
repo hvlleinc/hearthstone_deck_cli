f8f7ee91e6593d45c11addf1fb8d711e75987046
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2021-02-07 20:57:31 -0500

    search by keyword method is returning correct data

--- lib/hearthstone_deck_cli/api.rb
+++ lib/hearthstone_deck_cli/api.rb
@@ -24,17 +24,48 @@ require "pry"
       attr_accessor :url
       attr_reader :cardCount
     def initialize(url)
-      @url = "https://us.api.blizzard.com/hearthstone/cards?set=standardlocale=en_US&pageSize=10"
-      @headers = {"Authorization" => "Bearer US9AEn9hnxs521REWaIvHp4xHGO5bzSc4E" }
+      @url = "https://us.api.blizzard.com/hearthstone/cards?"
+      @headers = {"Authorization" => "Bearer USwhHfJv275CYeVUAjrqqK3O85Ilk1AuPa" }
     end 
     ## need something like this for all the diff searches
 
-    def search_by_class(classes)
-      data = HTTParty.get("#{@url}&class=#{classes}", headers: @headers)
-  
-      # puts data["cards"].first
+    def search_by_keyword(user_input)
+      # data = HTTParty.get("#{@url}/keyword#{user_input}", headers: @headers)
+      # data = HTTParty.get("#{@url}keyword=#{user_input}&set=standard", headers: @headers)
+      data = HTTParty.get("#{@url}locale=en_US&set=standard&keyword=#{user_input}&page=1&pageSize=10", headers: @headers)
+      ## we loop here to create cards
+      puts data["cards"].first.keys
+      # binding.pry
+      # while data["cardCount"] < 10
+       
+          
+      #     puts data["cards"].first["name"]
+      #     binding.pry
+      
+      # end
+    end 
 
-    end
+
+    def search_by_type(user_input)
+      # data = HTTParty.get("#{@url}/keyword#{user_input}", headers: @headers)
+      # data = HTTParty.get("#{@url}keyword=#{user_input}&set=standard", headers: @headers)
+      data = HTTParty.get("#{@url}locale=en_US&set=standard&keyword=#{user_input}&page=1&pageSize=10", headers: @headers)
+      data = HTTParty.get("#{@url}locale=en_US&set=standard&minionType=dragon&page=1&pageSize=10, headers
+      ## we loop here to create cards
+      puts data["cards"].first.keys
+      # binding.pry
+      # while data["cardCount"] < 10
+       
+          
+      #     puts data["cards"].first["name"]
+      #     binding.pry
+      
+      # end
+    end 
+
+
+  end
+
       #returning an array of all the cards
 
       # data.keys => returns
@@ -59,19 +90,19 @@ require "pry"
         
       ## we loop here to create cards
     # deck = Deck.new(deck_hash)
-  end
-
-
-    
   
 
+    
+  cards = Api.new("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&keyword=rush&access_token=USwhHfJv275CYeVUAjrqqK3O85Ilk1AuPa")
+ cards.search_by_keyword("rush")
+puts cards
   # malygos = Api.new("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&textFilter=malygos")
   # malygos.search_by_keyword("malygos")
 
   
   
- cards = Api.new("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=standard&class=mage&access_token=US9AEn9hnxs521REWaIvHp4xHGO5bzSc4E")
- cards.search_by_class("mage")
+#  cards = Api.new("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=standard&class=mage&access_token=US9AEn9hnxs521REWaIvHp4xHGO5bzSc4E")
+#  cards.search_by_class("mage")
 
 
 # class Cards::Api 
@@ -159,112 +190,7 @@ require "pry"
 
 
 
-## keyword comes from user input (searching for whatever)
-## works same as Djs pokemon name search
-# def search_by_keyword
-#     a = HTTParty.get("https://us.api.blizzard.com/hearthstone/cards?keyword=rush", headers: {"Authorization" => "Bearer USLaB8I9TQhaY62HBmjuvokklKqmPH3WQB" })
-#     ## cli will have a atrr accessor of a deck -> @deck = [] attr_accessor :deck 
-#     ## flow of app 
-#     ## 1. ask a user to search 
-#     ## call api to search for card on blizzard 
-#     ## create card objs, (create a loop to run 10 times so we create ten cards for them to search from )
-#     ## ask use which one to add to the deck 
-#     ## ask for another search
 
-    # class Api 
-
-    #     # attr_accessor :url, :headers
-
-      
-    #     # binding.pry
-    #     def initialize(url)
-    #       @url = "https://us.api.blizzard.com/hearthstone/cards?"
-    #       @headers = {"Authorization" => "Bearer USwhHfJv275CYeVUAjrqqK3O85Ilk1AuPa" }
-    #     end 
-     
-    #   ## need something like this for all the diff searches
-    #   def search_by_keyword(user_input)
-    #     data = HTTParty.get("#{@url}/keyword#{user_input}", @headers: headers)
-    #     ## we loop here to create cards
-    #     puts data
-    #   end 
-    # end 
-      # binding.pry
-#take account of the card count
-    #   def cardCount(cardcount)
-        
-    #     @cardCount = cardCount
-       
-    #   end
-    #   puts @cardCount
-    # end
-# base_url = "https://us.api.blizzard.com/hearthstone/cards?locale=en_US&textFilter=malygos&access_token=US9AEn9hnxs521REWaIvHp4xHGO5bzSc4E"
-# puts base_url
-
-
-  #   class Api 
-
-  #     attr_accessor :url
-
-  #     def initialize(url)
-  #       # @url = "https://us.api.blizzard.com/hearthstone/cards?"
-  #       # @headers = {"Authorization" => "Bearer USwhHfJv275CYeVUAjrqqK3O85Ilk1AuPa" }
-  #     end 
-  #     ## need something like this for all the diff searches
-  #   #   def search_by_keyword(user_input)
-  #   #     data = HTTParty.get("#{@url}/keyword#{user_input}", @headers: headers)
-  #   #     ## we loop here to create cards
-  #   #   end 
-
-  # #     def search_by_name(user_input)
-
-  # #       req_url = "#{@url}textFilter=#{user_input}"
-  # #       binding.pry
-  # #     data HTTParty.get(req_url)
-  # #     end
-  # # end 
-    
-  #   end
-
-  # Api.new("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&textFilter=malygos&access_token=US9AEn9hnxs521REWaIvHp4xHGO5bzSc4E")
-
-# api = Api.new("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&sort=name&access_token=US9AEn9hnxs521REWaIvHp4xHGO5bzSc4E")
-    # api = Api.new("https://us.api.blizzard.com/hearthstone/cards?")
-    # binding.pry
-  #  api.search_by_name("malygos")
-    # api.search_by_keyword("battlecry")
-  #   def search_by_name(name)
-  #      name_url = "#{@url}/class=mage&set=wild&sort=name&textFilter=#{name}"}
-  # #      binding.pry
-      
-       
-  
-  #     end
-
-  
-       
-  #     binding.pry
-       
-       
-  #     #  c = MyHearthstoneCards.new(deck_hash)
-  #       # deck = MyHearthstoneDeck.new(deck_hash)
-  # end
-  
-
-  
-
-
-
-
-      
-
-
-    #this works - need to break it down into smaller parts - 
-  
-
-  #  api = Api.new("https://us.api.blizzard.com/hearthstone/cards?locale=en_US&pageSize=10&access_token=US9AEn9hnxs521REWaIvHp4xHGO5bzSc4E")
-  #  api.search_by_name("frostbolt")
-    
     
 
 
